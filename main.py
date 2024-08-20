a4220bd4e0f2dd1099ec45b12dbaa8bd294eb550
Hans Thoft Rasmussen <72069575+hthoft@users.noreply.github.com>
2024-08-20 09:07:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,11 @@ def custom_print(*args, **kwargs):
     pass
 
 # Temporarily replace the print function
-original_print = print
-print = custom_print
+sys.stdout.write = custom_print
 
 if __name__ == '__main__':
-    # Your webview code here
     webview.create_window('Hello World', 'https://www.example.com')
     webview.start()
 
-# Restore the original print function
-print = original_print
+# Restore the original stdout write function
+sys.stdout.write = sys.__stdout__.write
