4bbbb223fed0d0db5523cf4e954a1e8b11d6f1d3
Hans Thoft Rasmussen <72069575+hthoft@users.noreply.github.com>
2024-08-20 10:26:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import webview
 import os
 
+
 if __name__ == '__main__':
     # Get the current directory
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -8,8 +9,7 @@ if __name__ == '__main__':
     # Specify the path to your index.html file
     html_file = os.path.join(current_dir, 'index.html')
 
-    # Create a webview window and open in fullscreen mode
-    webview.create_window('Local Webview Example', html_file, fullscreen=True)
-
-    # Start the webview
+    # Create a webview window to open the local HTML file
+    webview.create_window('Local Webview Example', html_file)
     webview.start()
+
